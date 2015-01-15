80c1defb2e31d2cb55cca6680c1cf0bc34008864
Elvis Wohlken <elvis@Elviss-MacBook-Pro.local>
2015-01-14 17:08:35 -0800

    Stripped out foundation components and switched out libsass for compass

--- GruntFile.js
+++ GruntFile.js
@@ -2,35 +2,26 @@ module.exports = function(grunt){
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
         path: {
-          root: "dev/",
-          css: "dev/css/",
-          scss: "dev/scss/",
-          js: "dev/js/",
-          html: "dev/views/",
-          haml: "dev/views/haml/",
-          deployCss: "dev/css/",
-          deployJs: "dev/js/",
+          root: "/",
+          deploy: "/",
+          css: "css/",
+          scss: "scss/",
+          js: "js/",
+          html: "views/",
+          haml: "views/haml/",
+          deployCss: "css/",
+          deployJs: "js/",
           host: "alignmenthealthcare.gscadmin.com"
         },
-        sass:{
+        compass:{
             app: {
                 options: {
-                    style: 'expanded'
-                },
-                files: [
-                    {
-                        "<%= path.css %>app.css" : "<%= path.scss %>app.scss",
-                        "<%= path.css %>app-ie.css" : "<%= path.scss %>app-ie.scss"
-                    }
-                ]
-            },
-            foundation: {
-              options: {
-                style: 'expanded'
-              },
-              files: [
-                {   "<%= path.css %>foundation.css" : '<%= path.scss %>foundation.scss' }
-              ]
+                    cssDir: '<%= path.css %>',
+                    sassDir: '<%= path.scss %>',
+                    outputStyle: 'expanded',
+                    require: 'sass-globbing',
+                    sourcemap: true
+                }
             }
         },
         watch: {
@@ -48,22 +39,10 @@ module.exports = function(grunt){
               tasks:[
                 'app'
               ]
-            },
-            foundation: {
-              options: {
-                livereload: true
-              },
-              files: [
-                '**/foundation.scss',
-                '**/foundation/*.scss'
-              ],
-              tasks:[
-                'foundation'
-              ]
             }
         },
         'ftp-deploy': {
-            app: {
+            css: {
               auth: {
                 host: '<%= path.host %>',
                 port: 21,
@@ -73,16 +52,6 @@ module.exports = function(grunt){
               dest: '<%= path.deployCss %>',
               exclusions: ['<%= path.css %>foundation.css.map', '<%= path.css %>foundation.css']
             },
-            foundation: {
-              auth: {
-                host: '<%= path.host %>',
-                port: 21,
-                authKey: 'key1'
-              },
-              src: '<%= path.css %>',
-              dest: '<%= path.deployCss %>',
-              exclusions: ['<%= path.css %>app-ie.css','<%= path.css %>app.css', '<%= path.css %>app.css.map', '<%= path.css %>app-ie.css.map']
-            },
             all: {
               auth: {
                 host: '<%= path.host %>',
@@ -90,7 +59,7 @@ module.exports = function(grunt){
                 authKey: 'key1'
               },
               src: '<%= path.root %>',
-              dest: '<%= path.root %>'
+              dest: '<%= path.deploy %>'
             }
 
         },
@@ -126,17 +95,16 @@ module.exports = function(grunt){
         }
     });
 
-    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-compass');
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-ftp-deploy');
     grunt.loadNpmTasks('grunt-spritesmith');
     grunt.loadNpmTasks('grunt-haml');
     grunt.loadNpmTasks('grunt-prettify');
 
-    grunt.registerTask('foundation', 'Compiles and Deploys foundation', ['sass:foundation','ftp-deploy:foundation']);
-    grunt.registerTask('app', 'Compiles and Deploys app', ['sass:app','ftp-deploy:app']);
+    grunt.registerTask('scss', 'Compiles scss', ['compass:app']);
+    grunt.registerTask('app', 'Compiles and Deploys css', ['sass:app','ftp-deploy:css']);
     grunt.registerTask('html', 'Compiles and Prettifies HAML', ['haml','prettify']);
 
     grunt.registerTask('default', ['watch:app']);
-  require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTas
 }
